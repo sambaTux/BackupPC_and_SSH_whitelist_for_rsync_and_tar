08b544f78f944488b70013742c0a40c82c92aa16
Robert Schoen <sambatux@web.de>
2015-05-27 18:15:58 +0200

    added lic.

--- rsync_white.sh
+++ rsync_white.sh
@@ -13,6 +13,15 @@
 #                commands, this script may be used. Thus, this script is intended to run on the backup clients.
 #                In fact, this script works like a white list for rsync commands.
 
+# LICENSE      : Copyright (C) 2011 Robert Schoen
+
+#                This program is free software: you can redistribute it and/or modify it under the terms 
+#                of the GNU General Public License as published by the Free Software Foundation, either 
+#                version 3 of the License, or (at your option) any later version.
+#                This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+#                without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#                See the GNU General Public License for more details. [http://www.gnu.org/licenses/]
+
 
 #####################################################################
 # ADVICE 1: There are some (syntax) rules to follow, in order to use this script.
